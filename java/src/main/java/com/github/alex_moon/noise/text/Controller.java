8b3fe6e6a2fdbe139ad01f092767a2240ae940d7
Alex Moon <alex.moon@tangentlabs.co.uk>
2014-02-10 08:41:29 +1100

    Having allowed myself to indulge in a ragequit for the holidays I've decided to reapproach this project from a different angle: JAVA!
    I've also decided since Java is distributed anyway to fuck Redis for a while. Keep it simple.

---
+++ java/src/main/java/com/github/alex_moon/noise/text/Controller.java
@@ -0,0 +1,8 @@
+package com.github.alex_moon.noise.text;
+
+public class Controller {
+    public Text getOneText() {
+        Text result = new Text("LOL it's actually just Hello World");
+        return result;
+    }
+}
---
+++ java/src/main/java/com/github/alex_moon/noise/text/Text.java
@@ -0,0 +1,13 @@
+package com.github.alex_moon.noise.text;
+
+public class Text {
+    private String stringValue = "";
+
+    public Text(String initialStringValue) {
+        stringValue = initialStringValue;
+    }
+
+    public String toString() {
+        return stringValue;
+    }
+}
